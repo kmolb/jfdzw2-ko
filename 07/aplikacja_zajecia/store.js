bed89f7e6b6ad6e466b842d08ff95ca9aa2c0075
Kamil Olber <olb.kamil@gmail.com>
2018-10-26 22:52:12 +0200

    praca domowa 07 - aplikacja zajecia

---
+++ 07/aplikacja_zajecia/store.js
@@ -0,0 +1,232 @@
+"use strict";
+
+const store = {
+  // funkcja wywoływana na zdarzenie kliknięcia [V zatwierdź]
+  // w sekcji 'nowy produkt'
+  addNewItem: function(item) {
+    
+
+
+    // pkt 3 dodaj walidację na store.addNewItem, aby nie można było dodać produktu bez nazwy //
+
+   ///  
+   // const name = this.name ; 
+   // if(name.value.length <= 0 ){
+   // alert('brak produktu');
+   //   return false;
+   //}
+
+
+    const todos = this.getData("todos");
+
+      
+    item.checked = false;
+    todos.push(item);
+
+    this.setData("todos", todos);
+    appStart(todos);
+  },
+
+  // funkcja wywoływana na zdarzenie kliknięcia checkboxa [ ] lub [X]
+  // na elemencie listy 'do kupienia' i 'kupione'
+  updateItem: function(item) {
+    const todos = this.getData("todos");
+
+    const foundItem = todos.find(x => x.name === item.name);
+    foundItem.checked = item.checked;
+
+    this.setData("todos", todos);
+    appStart(todos);
+  },
+
+  // funkcja wywoływana na zdarzenie kliknięcia usuń [X]
+  // na elemencie listy 'do kupienia' i 'kupione'
+  deleteItem: function(item) {
+    // znajdź element item w tablicy todos i go usuń z tablicy
+    // item ma strukturę: {checked: boolean, name: string,
+    // category: string, shops: [string]}
+    const todos = this.getData("todos");
+
+    // za pomocą funkcji find znajdz element spełniający warunek
+    // item.name === x.name
+    // przypisz ten element do zmiennej
+    const found = todos.find(x => x.name === item.name);
+    const indexOfItem = todos.indexOf(found);
+    // za pomocą funkcji splice usuń element z tablicy todos
+    todos.splice(indexOfItem, 1);
+
+    this.setData("todos", todos);
+    appStart(todos);
+  },
+
+  // funkcja wywoływana na zdarzenie kliknięcia przycisku
+  // [zaznacz wszystkie] w sekcji 'multiakcje'
+  checkAll: function() {
+    // zmień właściwość checked wszystkich obiektów tablicy
+    // todos na true
+    const todos = this.getData("todos");
+
+    // for(let i = 0; i < todos.length; i++ ){
+    //   todos[i].checked = true;
+    // }
+    todos.forEach(x => (x.checked = true));
+
+    this.setData("todos", todos);
+    appStart(todos);
+  },
+
+  // funkcja wywoływana na zdarzenie kliknięcia przycisku [odznacz wszystkie]
+  // w sekcji 'multiakcje'
+  uncheckAll: function() {
+    // zmień właściwość checked wszystkich obiektów tablicy todos na false
+    const todos = this.getData("todos");
+
+    // implementacja
+    todos.forEach(x => (x.checked = false));
+
+    this.setData("todos", todos);
+    appStart(todos);
+  },
+
+  // funkcja wywoływana na zdarzenie kliknięcia przycisku [zamień zaznaczenie]
+  // w sekcji 'multiakcje'
+  switchSelection: function() {
+    // przełącz właściwość checked wszystkich obiektów tablicy todos
+    // true zamień na false, a false na true
+    const todos = this.getData("todos");
+    //todos.forEach(x => x.checked ? false : true);
+    todos.forEach(x => (x.checked = !x.checked));
+
+    this.setData("todos", todos);
+    appStart(todos);
+  },
+
+  // funkcja wywoływana na zdarzenie kliknięcia przycisku [usuń wszystkie]
+  // w sekcji 'multiakcje'
+  deleteAll: function() {
+    // usuń wszystkie elementy tablicy todos
+    const todos = this.getData("todos");
+
+    todos = [];
+    // todos.length = 0;
+    // todos.splice(0, todos.length);
+
+    this.setData("todos", todos);
+    appStart(todos);
+  },
+
+  // funkcja wywoływana na zdarzenie kliknięcia przycisku [usuń kupione]
+  // w sekcji 'multiakcje'
+  deleteBought: function() {
+    // usuń elementy tablicy todos, które spełniają warunek checked === true
+    let todos = this.getData("todos");
+
+    todos = todos.filter(x => !x.checked);
+
+    this.setData("todos", todos);
+    appStart(todos);
+  },
+
+  // funkcja wywoływana na zdarzenie kliknięcia [V zatwierdź]
+  // w sekcji 'filtry'
+  filterItems: function(filter) {
+    // przefiltruj tablicę todos względem obiektu wejściowego filter
+    // obiekt filter ma strukturę: { name: string, category: string, shops: [string] }
+    // żeby sprawdzić czy waunek dla shops jest prawdziwy sprawdź, czy jakikolwiek
+    // przesłany element znajduje się w obiekcie tabeli todos (funkcja some)
+    let todos = this.getData("todos");
+    // 'cola'.indexOf('co') === 0;
+    // 'cola'.indexOf('la') === 2;
+    // 'cola'.indexOf('lk') === -1;
+    todos = todos
+      .filter(x => !filter.name || x.name.indexOf(filter.name) !== -1)
+      
+
+
+
+    // pkt 1 store.filter //
+
+
+
+          .filter(item => !filter.category || item.category === filter.category)
+          .filter(
+            item =>
+              !filter.shops || filter.shops.length === 0 || item.shops.some(i => filter.shops.find(f => f === i))
+          );
+
+        appStart(todos);
+      },
+
+  // funkcja wywoływana na zdarzenie kliknięcia przycisku [sortuj po nazwie],
+  // [sortuj po kategorii] i [sortuj po sklepach] w sekcji 'multiakcje'
+  sort: function(prop) {
+    // posortuj tablicę todos względem parametru przekazanego w atrybucie prop
+    // props == 'name' || 'category' || 'shops'
+    // obj.name === obj['name']
+    const todos = this.getData("todos")
+
+
+
+    
+        // pkt 1 store.sort //
+
+
+
+            .sort((a, b) => a[prop].toString().localeCompare(b[prop].toString()));
+            
+
+            this.setData("todos", todos);
+            appStart(todos);
+          },
+
+  // funkcja jest wywoływana podczas renderowania aplikacji (po każdej zmianie na liście)
+  // funkcja zwraca listę obiektów o strukturze: { name: string, count: number }, gdzie
+  // name to nazwa kategorii, a count to liczba produktów z listy do kupienia w tej kategorii
+  getCategoriesSummary: function() {
+    return this.getData("todos");
+    // optumalnie należy najpierw należy przefiltrować tablicę, następnie ją zredukować do obiektu
+    // potem wykorzystać funkcję przetwarzania obiektów by zmapować entries, a następnie posortować listę
+  },
+
+  // funkcja jest wywoływana podczas renderowania aplikacji (po każdej zmianie na liście)
+  // funkcja zwraca listę obiektów o strukturze: { name: string, count: number }, gdzie
+  // name to nazwa sklepu, a count to liczba produktów z listy do kupienia w tym sklepie
+  getShopsSummary: function() {
+    return this.getData("todos");
+    // optumalnie należy najpierw należy przefiltrować tablicę, następnie ją zredukować do obiektu
+    // potem wykorzystać funkcję przetwarzania obiektów by zmapować entries, a następnie posortować listę
+  },
+
+  // funkcja jest wywoływana na kliknięcie w konkretny sklep
+  // na podsumowaniu listy zakupów (między sekcją 'do kupienia' i 'kupione')
+  // do funkcji jest przekazywana nazwa aklepu, który został kliknięty
+  // zwróć listę stringów (nazw produktów), które są do kupienia w danym slepie
+  // listę posortuj alfabetycznie
+  getItemNamesContainingShop: function(shop) {
+    return this.getData("todos");
+    // filter
+    // map
+  },
+
+  // funkcja pobierająca dane o podanym kluczu
+  // dostępne klucze: 'todos', 'categories', shops'
+  getData: function(key) {
+    return JSON.parse(localStorage.getItem(key) || "[]");
+  },
+
+  // funkcja aktualizująca dane o podanym kluczu i wartości
+  // dostępne klucze: 'todos', 'categories', shops'
+  // przyjmowane wartości: tablice danych:
+  // categories: tablica stringów
+  // shops: tablica stringów
+  // todos: tablica obiektów o strukturze:
+  // {
+  //   checked: boolean,
+  //   name: string,
+  //   category: string z dostępnych w tablicy categories
+  //   shops: tablica stringów z dostępnych w tablicy shops
+  // }
+  setData: function(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+};
